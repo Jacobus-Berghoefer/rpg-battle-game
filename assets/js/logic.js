5a590e6b821df2870850569790a0a8f5fe7e25cb
Jacobus <jacobus.berghoefer@gmail.com>
2025-01-14 12:52:16 -0600

    adding zone0 html, and additional code for newgame loadgame

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,32 +1,31 @@
 // Game Logic
-
-
-
+//variables
 let playerData = parseInt(localStorage.getItem('userCharacter')) || {
-    userCharacter: {name: 'characterName', Level: 1},
+    userCharacter: {name: 'characterName', Level: 1, stats:{Str: 1, Stam: 1, Mana: 1}, inventory: [], location:'zone0' },
 };
 
 //Create new game for user
 function newGame(){
     localStorage.clear();
+    playerData = parseInt(localStorage.getItem('userCharacter')) || {
+        userCharacter: {name: 'characterName', Level: 1, stats:{Str: 1, Stam: 1, Mana: 1}, inventory: [], location:'zone0' },
+    };
+    localStorage.setItem('userCharacter', JSON.stringify(playerData));
     console.log(`local storage cleared`);
 }
 
 
-//Load game data from local storage if a user has saved
+//Load game data from local storage if a user has saved data
 function loadGame() {
-    const gameData = localStorage.getItem('gameData');
+    let gameData = localStorage.getItem('userCharacter');
 
-    if (gameData)
-        const parsedData = JSON.parse(gameData);
-    
-    console.log('Game data loaded', parsedData);
-
-    //Need to update player based on saved data (stats, inventory, location)
-    if (parsedData.player) {
-        updatePlayer(parsedData.player);
-    }
-    else {
+    if (gameData) {
+        //Need to update player based on saved player data (stats, inventory, location)
+        playerData = JSON.parse(gameData);
+        updatePlayer(playerData);
+        console.log('Game data loaded', playerData);
+    } else {
+        playerData = 'null';
         console.log('No saved game data found.');
     }
 }
@@ -34,6 +33,13 @@ function loadGame() {
 //functions to update game state
 
 //update player state to reflect states, inventory, location
-function updatePlayer(playerData) {
-    console.log('Player Stats:')
+function updatePlayer(data) {
+    console.log('Player Stats:', data.userCharacter)
+    //Add function details to update a players stats, inventory, location here
+}
+
+//save player game data
+function saveGame() {
+    localStorage.setItem('userCharacter',JSON.stringify(playerData));
+    console.log('Game Data saved:', playerData);
 }
