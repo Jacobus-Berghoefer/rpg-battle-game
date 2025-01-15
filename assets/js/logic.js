cd8dcf3ac7f12247633c21bf116e3924b46a2ac6
Jacobus <jacobus.berghoefer@gmail.com>
2025-01-14 19:40:19 -0600

    main menu and options updates

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,45 +1,87 @@
 // Game Logic
+
 //variables
 
-//let playerData = parseInt(localStorage.getItem('userCharacter')) || {
-//    userCharacter: {
-//        name: 'characterName', 
-//        Level: 1, 
-//        stats:{Str: 1, Stam: 1, Mana: 1}, 
-//        inventory: [], 
-//        location:'zone0' 
-//    },
-//};
+let playerData = parseInt(localStorage.getItem('userCharacter')) || {
+    userCharacter: {
+        name: 'characterName', 
+        Level: 1, 
+        stats:{Str: 1, Stam: 1, Mana: 1}, 
+        inventory: [], 
+        location:'zone0' 
+    },
+};
+
+//Zone Locations
+let dataZone = {
+   //let zoneLocation = JSON.parse(localStorage.getItem(playerData.userCharacter.location));
+
+    mainMenu: 'index.html',
+    zone0: '/assets/zones/zone0.html',
+    zone1: '/assets/zones/zone1.html',
+}
 
 //Create new game for user
 function newGame(){
     localStorage.clear();
-//    playerData = parseInt(localStorage.getItem('userCharacter')) || {
-//        userCharacter: {
-//            name: 'characterName', 
-//            Level: 1, 
-//            stats:{Str: 1, Stam: 1, Mana: 1}, 
-//            inventory: [], 
-//            location:'zone0' 
-//        },
-//    };
-//    localStorage.setItem('userCharacter', JSON.stringify(playerData));
-//    console.log(`New Game Started!`, playerData);
-    console.log(`New Game Started!`);
+    playerData = parseInt(localStorage.getItem('userCharacter')) || {
+        userCharacter: {
+            name: 'characterName', 
+            Level: 1, 
+            stats:{Str: 1, Stam: 1, Mana: 1}, 
+            inventory: [], 
+            location:'zone0' 
+        },
+    };
+    localStorage.setItem('userCharacter', JSON.stringify(playerData));
+    console.log(`New Game Started!`, playerData);
+    //console.log(`New Game Started!`);
+    alert('Good Luck On Your New Adventure!');
 
-    window.location.href = './assets/zones/zone0.html';
+    window.location.href = './assets/zones/charCreate.html';
 }
 
+function getCurrentZone() {
+    return document.documentElement.getAttribute("data-zone");
+}
+
+//save player game data
+function saveGame() {
+
+    //what element to give attribute of 'data-zone' in html sheet so it references the zone location?
+    //const currentZone = document.body.getAttribute("data-zone");
+
+    const currentZone = getCurrentZone();
+    playerData.userCharacter.location = currentZone;
+
+    localStorage.setItem('userCharacter',JSON.stringify(playerData));
+    console.log('Game Data saved:', playerData);
+    alert(`Game Saved! Current Zone: ${currentZone}`);
+}
 
 //Load game data from local storage if a user has saved data
 function loadGame() {
     let savedData = localStorage.getItem('userCharacter');
 
     if (savedData) {
+
         //Need to update player based on saved player data (stats, inventory, location)
         playerData = JSON.parse(savedData);
+
+        //get the saved zone location from local storage
+        let savedZone = playerData.userCharacter.location;
+
+        //reference the dataZone object to get the file path for the saved zone
+        let currentZone = dataZone[savedZone];
+
         //updatePlayer(playerData); //ADD DETAILS TO UPDATE PLAYER FUNCTION
         console.log('Game data loaded', playerData);
+
+    if (currentZone) {
+        window.location.href = currentZone;
+        } else {
+            alert('Error: Saved zone location not found!');
+        }        
     } else {
         alert('No saved game found!');
         console.log('No saved game data found.');
@@ -63,12 +105,18 @@ function exitGame() {
     }
 }
 
+function mainMenu() {
+   
+    console.log('You have moved back to the Main Menu.');
+    alert('You have moved back to the Main Menu.');
+    window.location.href = '../../index.html';
+}
+
 function nextZone() {
    
     console.log('You have moved to Zone 1.');
     alert('You have moved to Zone 1.');
-
-    window.location.href = './assets/zones/zone1.html';
+    window.location.href = './zone1.html';
 }
 
 
@@ -80,8 +128,5 @@ function updatePlayer(data) {
     //Add function details to update a players stats, inventory, location here
 }
 
-//save player game data
-function saveGame() {
-    localStorage.setItem('userCharacter',JSON.stringify(playerData));
-    console.log('Game Data saved:', playerData);
-}
+
+
